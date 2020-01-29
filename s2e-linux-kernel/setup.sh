4afbfc9e48fa1ea3e448176de93f2eb12a20caea
CvvT <weitengchencc@gmail.com>
2020-01-28 17:23:03 -0800

    update

--- s2e-linux-kernel/setup.sh
+++ s2e-linux-kernel/setup.sh
@@ -1,13 +1,19 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
 
-wget https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/linux-4.9.3.tar.gz
-tar -xvf linux-4.9.3.tar.gz
-cd linux-4.9.3
-patch -p 1 -i ../patch-4.9.3
-patch -p 1 -i ../ilog2_NaN.patch
+VERSION=$1
+if [[ ! -f "patch-${VERSION}" ]]; then
+    echo "Do not support version ${VERSION}" && exit 1
+fi
+
+# FIXME: 4.14.0
+wget https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/linux-${VERSION}.tar.gz
+tar -xzf linux-${VERSION}.tar.gz
+cd linux-${VERSION}
+patch -p 1 -i ../patch-${VERSION}
+patch -N -p 1 -i ../ilog2_NaN.patch || true
 cp -r ../../../s2e-linux-kernel/linux-4.9.3/kernel/s2e kernel/
 cp -r ../../../s2e-linux-kernel/include ../
-cp ../config_4.9.3 .config
-rm ../linux-4.9.3.tar.gz
+cp ../config_${VERSION} .config
+rm ../linux-${VERSION}.tar.gz
