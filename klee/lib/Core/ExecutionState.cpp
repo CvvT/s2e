b3d56e198b1133e6f6264971e3c9c319f46b84ae
CvvT <weitengchencc@gmail.com>
2020-03-23 16:02:09 -0700

    add supress option

--- klee/lib/Core/ExecutionState.cpp
+++ klee/lib/Core/ExecutionState.cpp
@@ -49,7 +49,7 @@ cl::opt<bool> DebugPrintInstructions("debug-print-instructions", cl::desc("Print
 // This is set to false in order to avoid the overhead of printing large expressions
 cl::opt<bool> PrintConcretizedExpression("print-concretized-expression", cl::desc("Print concretized expression."),
                                          cl::init(false));
-
+cl::opt<bool> PrintConcretizedReason("print-concretized-reason", cl::desc("Print concretization's reason."), cl::init(false));
 /***/
 
 StackFrame::StackFrame(KInstIterator _caller, KFunction *_kf) : caller(_caller), kf(_kf), callPathNode(0), varargs(0) {
@@ -491,25 +491,27 @@ ref<ConstantExpr> ExecutionState::toConstant(ref<Expr> e, const std::string &rea
     assert(isa<ConstantExpr>(evalResult) && "Must be concrete");
     value = dyn_cast<ConstantExpr>(evalResult);
 
-    // std::string s;
-    // raw_string_ostream os(s);
+    if (PrintConcretizedReason) {
+        std::string s;
+        raw_string_ostream os(s);
 
-    // os << "silently concretizing ";
+        os << "silently concretizing ";
 
-    // const KInstruction *ki = prevPC;
-    // if (ki && ki->inst) {
-    //     os << "(instruction: " << ki->inst->getParent()->getParent()->getName().str() << ": " << *ki->inst << ") ";
-    // }
+        const KInstruction *ki = prevPC;
+        if (ki && ki->inst) {
+            os << "(instruction: " << ki->inst->getParent()->getParent()->getName().str() << ": " << *ki->inst << ") ";
+        }
 
-    // os << "(reason: " << reason << ") ";
+        os << "(reason: " << reason << ") ";
 
-    // if (PrintConcretizedExpression) {
-    //     os << " expression " << e;
-    // }
+        if (PrintConcretizedExpression) {
+            os << " expression " << e;
+        }
 
-    // os << " to value " << value;
+        os << " to value " << value;
 
-    // klee_warning_external(reason.c_str(), "%s", os.str().c_str());
+        klee_warning_external(reason.c_str(), "%s", os.str().c_str());
+    }
 
     if (!addConstraint(EqExpr::create(e, value))) {
         abort();
